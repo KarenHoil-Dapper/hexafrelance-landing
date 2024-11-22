d2ae36096587749e0598a80c986b269078e4c6d2
Josué138 <c.oviedo@dappertechnologies.com>
2024-11-21 19:00:35 -0500

    Update all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { CookieService } from 'ngx-cookie-service';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './pages/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { WhyWeComponent } from './components/home/why-we/why-we.component';
@@ -20,14 +19,10 @@ import { OurProjectsComponent } from './components/home/our-projects/our-project
 import { YourPartnerComponent } from './components/home/your-partner/your-partner.component';
 import { BannerHomeComponent } from './components/home/banner-home/banner-home.component';
 import { BlueMoonHomeComponent } from './components/home/blue-moon-home/blue-moon-home.component';
-import { AboutUsComponent } from './pages/about-us/about-us.component';
-import { InvestmentComponent } from './pages/investment/investment.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { SecondSectionComponent } from './components/second-section/second-section.component';
 import { WhatAndHowAboutComponent } from './components/about-us/what-and-how-about/what-and-how-about.component';
 import { WhatAndHowInvestmentComponent } from './components/investment/what-and-how-investment/what-and-how-investment.component';
 import { InformationInvestmentComponent } from './components/investment/information-investment/information-investment.component';
-import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { DevelopmentComponent } from './pages/development/development.component';
 import { BannerBlueComponent } from './components/blue-moon/banner-blue/banner-blue.component';
 import { SwiperBlueComponent } from './components/blue-moon/swiper-blue/swiper-blue.component';
@@ -37,16 +32,13 @@ import { BlueMoonDedicatedComponent } from './components/blue-moon/blue-moon-ded
 import { CharacteristicsBlueComponent } from './components/blue-moon/characteristics-blue/characteristics-blue.component';
 import { TestimonialsHomeComponent } from './components/home/testimonials-home/testimonials-home.component';
 import { BannersComponent } from './components/banners/banners.component';
-import { ThankYouPageComponent } from './pages/thank-you-page/thank-you-page.component';
 
 import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent,
     AppComponent,
-    HomeComponent,
     NavBarComponent,
     FooterComponent,
     WhyWeComponent,
@@ -54,14 +46,10 @@ import { ToastrModule } from 'ngx-toastr';
     YourPartnerComponent,
     BannerHomeComponent,
     BlueMoonHomeComponent,
-    AboutUsComponent,
-    InvestmentComponent,
-    ContactComponent,
     SecondSectionComponent,
     WhatAndHowAboutComponent,
     WhatAndHowInvestmentComponent,
     InformationInvestmentComponent,
-    ContactFormComponent,
     DevelopmentComponent,
     BannerBlueComponent,
     SwiperBlueComponent,
@@ -71,7 +59,6 @@ import { ToastrModule } from 'ngx-toastr';
     CharacteristicsBlueComponent,
     TestimonialsHomeComponent,
     BannersComponent,
-    ThankYouPageComponent,
   ],
   imports: [
     BrowserModule,
@@ -84,23 +71,10 @@ import { ToastrModule } from 'ngx-toastr';
     ReactiveFormsModule,
     MaterialModule,
     SwiperModule,
-    NgbModule,
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-center',
-      preventDuplicates: true,
-      closeButton: true,
-      progressBar: true,
-      iconClasses: {
-        error: 'icon-error',
-        info: 'icon-info',
-        success: 'icon-success',
-        warning: 'icon-warning'
-      }
-    })
   ],
-  providers: [  
+  providers: [
     UrlLocalStorageService,
-    CookieService,  
+    CookieService,
     ReCaptchaV3Service,
     {
       provide: RECAPTCHA_V3_SITE_KEY,
